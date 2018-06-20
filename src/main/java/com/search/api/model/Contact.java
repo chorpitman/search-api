b68e6928d4625d333afd77f78f7152c886f72d3a
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-06-20 11:53:29 +0300

    added model

---
+++ src/main/java/com/search/api/model/Contact.java
@@ -0,0 +1,32 @@
+package com.search.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "contacts")
+public class Contact {
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+}
